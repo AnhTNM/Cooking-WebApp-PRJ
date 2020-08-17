873f4ca288e85ce31d96f50fe0434073b542483c
thangpham <thangpham982708@gmail.com>
2020-08-18 01:13:32 +0700

    thang

--- home.js
+++ home.js
@@ -1,13 +1,23 @@
 const bodyEl = document.getElementById("body1")
-const foodData = [{
-    name: "Bánh tôm hùm Luke",
-    description: "Banh Tom Hum Lake",
-    img: "./cook/b.jpg"
-},
+const foodData = [
+
+
 
 {
-    name: "Bánh bông lan Castella",
+    name: "Cánh gà chiên bơ tỏi",
+    description: "Cánh gà chiên bơ tỏi là món ăn đơn giản nhưng sẽ mang lại một bữa ngon miệng, hấp dẫn cho gia đình bạn. Lớp da bên ngoài vàng giòn, thơm lừng mùi bơ, phần thịt gà bên trong thì chín mềm, có vị ngọt đặc trưng của thịt gà. Thêm một món ăn nữa dành cho cả nhà cùng thưởng thức rồi, bạn vào bếp để xem cách làm ngày nhé.",
+    img: "./cook/ga.jpg",
+    id : 0,
+},
+{
+    name: "Xôi cốm hạt sen",
     description: "Đây là bánh cuốn",
+    img: "./cook/xoi.jpg",
+    id : 1,
+},
+{
+    name: "Bánh bông lan Castella",
+    description: "Bánh bông lan cơ bản (sponge cake) Sponge Cake là một trong những loại bánh cơ bản và đơn giản nhất, bánh được làm bằng cách đánh bông trứng, không sử dụng bơ hoặc rất ít bơ, và cũng không cần bột nở vì bánh nở chủ yếu dựa vào bọt khí trong trứng đánh bông.",
     img: "./cook/n.jpg"
 },
 
@@ -84,17 +94,11 @@ const foodData = [{
 },
 
 
-{
-    name: "Xôi cốm hạt sen",
-    description: "Đây là bánh cuốn",
-    img: "./cook/xoi.jpg"
-},
-
 
 {
-    name: "Cánh gà chiên bơ tỏi",
-    description: "Đây là bánh cuốn",
-    img: "./cook/ga.jpg"
+    name: "Bánh tôm hùm Luke",
+    description: "Banh Tom Hum Lake",
+    img: "./cook/b.jpg"
 },
 
 
@@ -133,7 +137,7 @@ function renderFood(){
             <p class = "pic_anh_add"><i class="fas fa-utensil-spoon"></i> THÊM VÀO THƯC ĐƠN</p> 
             
             <div class="pic_txt">
-                <h3 onclick="assign()">${foodData[i].name}</h3>
+                <h3 onclick="assign(this)">${foodData[i].name}</h3>
                 <div><p>Time: 7.pm - 8.pm </p></div>
                 <div><p>Always Fresh</p></div>
             </div>
@@ -164,8 +168,9 @@ renderFood()
 function pageChange(){
     window.location.assign('food.html');
 }
-function assign(){
-    window.location.assign('detail.html');
+function assign(e){
+    console.log(foodData[1].id);
+    // window.location.assign('detail.html');
 }
 
 
